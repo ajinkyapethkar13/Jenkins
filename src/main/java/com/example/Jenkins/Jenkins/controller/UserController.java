12a2c38362efc52f3ce43a60ec129815f647b0fd
Ajinkya Pethkar <ajinkyapethkar@gmail.com>
2022-11-10 11:23:59 +0530

    Commit

---
+++ src/main/java/com/example/Jenkins/Jenkins/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.Jenkins.Jenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String greet(){
+        return "Hello Raj";
+    }
+}
