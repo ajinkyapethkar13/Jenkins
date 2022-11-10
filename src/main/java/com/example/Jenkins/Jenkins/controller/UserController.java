eb89516552b78431f6d488e0a59797a1479d6dc2
Ajinkya Pethkar <ajinkyapethkar@gmail.com>
2022-11-10 12:28:25 +0530

    Commit-10/11-12:18

--- src/main/java/com/example/Jenkins/Jenkins/controller/UserController.java
+++ src/main/java/com/example/Jenkins/Jenkins/controller/UserController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String greet(){
-        return "Hello Raj";
+        return "Hello Sham";
     }
 }
